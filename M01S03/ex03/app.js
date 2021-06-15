06ed9c36c7bc1d6c7ecfc56a8780d44c19dfe2c7
patris-patras <patras.patricia@gmail.com>
2021-06-15 21:48:59 +0300

    js obj s03 live

---
+++ M01S03/ex03/app.js
@@ -0,0 +1,29 @@
+var person = {
+  name: 'Patris',
+  suname: 'Patras',
+  friends: {},
+};
+
+var larry = {
+  name: 'Larry',
+  surname: 'Larryson',
+  friends: {},
+};
+
+var carol = {
+  name: 'Carol',
+  surname: 'Carolson',
+  friends: {},
+};
+
+var steven = {
+  name: 'Steven',
+  surname: 'Stevenson',
+  friends: {},
+};
+
+var adnra = {
+  name: 'Andra',
+  surname: 'Andrason',
+  friends: {},
+};
