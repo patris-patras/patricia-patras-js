c21af5d90562093655c4bbbef8419168ba8571b2
patris-patras <patras.patricia@gmail.com>
2021-08-11 16:10:28 +0300

    M02 S04 exc 04 - tema

--- M02S04/ex02/app.js
+++ M02S04/ex02/app.js
@@ -1,6 +1,10 @@
 const box = document.querySelector('.box');
 const controls = document.querySelectorAll('.controls').item(0);
 
+const colorInput = document.querySelector('.color-input');
+const changeColorButton = document.querySelector('.change-color-button');
+const resetColorButton = document.querySelector('.reset-button');
+
 // event delegation - vreau doar o functie nu 3
 controls.addEventListener('click', (event) => {
   console.log(event.target); // elem de pe care a plecat eventul/pe ce am dat click, (target ptr ca in unele cazuri pleaca si se intoarce pe elem targetat
@@ -18,3 +22,27 @@ controls.addEventListener('click', (event) => {
 
   box.style.backgroundColor = colorValue;
 });
+
+colorInput.addEventListener('input', () => {
+  const colorValue = colorInput.value;
+
+  if (colorInput.value.length > 0) {
+    changeColorButton.style.backgroundColor = colorValue;
+  }
+
+  changeColorButton.addEventListener('click', () => {
+    box.style.backgroundColor = colorValue;
+  });
+});
+
+resetColorButton.addEventListener('click', () => {
+  // colorInput.value = '';
+  // changeColorButton.style.backgroundColor = '';
+
+  colorInput.removeAttribute(`style`);
+  changeColorButton.removeAttribute(`style`);
+});
+
+// resetButton sa apara doar dupa ce e input
+
+//  HELP - nu imi iese asta cu removeAttribute.. nu imi dau seama cum trebuie scris
