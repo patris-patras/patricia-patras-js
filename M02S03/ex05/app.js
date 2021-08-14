9cf84bdda04dcba4d78fa983f08df23ac5e10f97
patris-patras <patras.patricia@gmail.com>
2021-08-14 12:58:48 +0300

    M02S03 exc 05 - last exc/homework

--- M02S03/ex05/app.js
+++ M02S03/ex05/app.js
@@ -4,21 +4,46 @@ const showMessage = (message) => {
 
   document.body.append(p);
 };
+
 let initialWidth = window.innerWidth;
+let initialHeight = window.innerHeight;
+let windowSurface = initialWidth * initialHeight;
+
+document.addEventListener('DOMContentLoaded', function () {
+  showMessage(`Fereastra are suprafata initiala de ${windowSurface} pixeli.`);
+});
 
 showMessage(`Latimea ferestrei este: ${initialWidth}`);
+showMessage(`Inaltimea ferestrei este: ${initialHeight}`);
+showMessage(`Fereastra are acum suprafata de ${windowSurface} pixeli.`);
 
 const debouncedResize = debounce(() => {
   const currentWidth = window.innerWidth;
+  const currentHeight = window.innerHeight;
   showMessage(`Latimea ferestrei este: ${currentWidth}`);
+  showMessage(`Inaltimea ferestrei este: ${currentHeight}`);
 
   if (currentWidth !== initialWidth) {
     showMessage(`Fereastra si-a schimbat latimea`);
   }
 
+  if (currentHeight !== initialHeight) {
+    showMessage(`Fereastra si-a schimbat inaltimea`);
+  }
+
+  windowSurface = currentHeight * currentWidth;
+  showMessage(`Fereastra are acum suprafata de ${windowSurface} pixeli.`);
+
   initialWidth = currentWidth;
-}, 200);
+  initialHeight = currentHeight;
+}, 500);
 
 window.addEventListener('resize', debouncedResize);
 
-// !!!!  REPARA ODATA CONSOLA !!!!!
+// Folosind evenimentul resize al obiectului window si proprietatile innerWidth si innerHeight,
+//  la fiecare redimensionare a ferestrei, afiseaza intr-un paragraf
+// mesajul: “Fereastra are acum suprafata de xxx pixeli.”.
+
+// Folosind evenimentul de DOMContentLoaded
+//si o functie reutilizabila afiseaza suprafata ferestrei
+//si la momentul incarcarii site-ului.
