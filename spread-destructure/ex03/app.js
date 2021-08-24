ab30110c060fb917707ec9eeced3968664470c9d
patris-patras <patras.patricia@gmail.com>
2021-08-24 20:23:59 +0300

    M02 Spread-destructure LIVE

--- spread-destructure/ex03/app.js
+++ spread-destructure/ex03/app.js
@@ -20,3 +20,50 @@ let age1 = {
 let age2 = {
   age: 25,
 };
+
+// In fisierul app.js Andra are trebui sa aiba 25 de ani, Steven 20 iar Rolf 40,
+// folosind operatorul spread, creaza obiecte de tipul fullPerson1, fullPerson2, si fullPerson3 care sa reflecte acest lucru
+const fullPerson1 = {
+  ...person1,
+  ...age1,
+};
+
+const fullPerson2 = {
+  ...person2,
+  age: 40, // pentru ca la asta nu am un obiect age cu care sa il concatenez
+};
+
+const fullPerson3 = {
+  ...person3,
+  ...age2,
+};
+
+console.log(fullPerson1, fullPerson2, fullPerson3);
+console.table(fullPerson1);
+
+// Folosind noul obiect fullPerson3 creaza intr-un obiect numit fullPerson4
+// cu numele de familie al Andrei care sa se numeasca Anita si sa aiba 36 de ani.
+const fullPerson4 = {
+  ...fullPerson3,
+  name: 'Anita', // suprascriem
+  age: 36,
+};
+console.log(fullPerson3, fullPerson4);
+
+// improv
+const calculateSurface = (...args) => {
+  const length = args.length;
+
+  if (length === 2) {
+    return args[0] * args[1];
+  }
+
+  if (length === 1) {
+    return args[0] ** 2;
+  }
+};
+
+const square = calculateSurface(2);
+const rectangle = calculateSurface(2, 6);
+
+console.log(square, rectangle);
--- spread-destructure/ex05/app.js
+++ spread-destructure/ex05/app.js
@@ -22,3 +22,40 @@ let person = {
     },
   ],
 };
+
+//  1.Folosind destructurarea salveaza primul si ultimul skill in variabile numite skill1 si skill3,
+// apoi folosind aceaste variabile, afiseaza intr-un paragraf propozitia: “Cunosc html si css.”.
+const [skill1, , skill3] = person.skills;
+/** echivalent:
+ * skill1 = person.skills[0];
+ * skill3 = person.skills[2];
+ */
+console.log(`Cunosc ${skill1} & ${skill3}`);
+
+// 2.Intr-un mod similar destructureaza arrayul friends pentru a obtine obiectul de pe pozitia 1
+// apoi destructureaza obiectul obtinut in variabilele friend2Name, friend2Surname si friend2Age
+// astfel incat sa le poti folosi in propozitia: Ma numesc Steven Stevenson si am 31 de ani.
+const [, friend2] = person.friends;
+// echivalent: friend2 = person.friends[1];
+const { name: friend2Name, surname: friend2Surname, age: friend2Age } = friend2; // "scoatem proprietatea age ca friend2Age"
+
+let message = `Ma numesc ${friend2Name} ${friend2Surname} si am ${friend2Age} ani.`;
+console.log(message);
+
+// V2
+const [, , { name: friend3Name, surname: friend3Surname }] = person.friends; // adica unde am var, in loc de ea pun variabila.proprietate
+console.log(`Prietena mea este ${friend3Name} ${friend3Surname}`);
+
+// 3.Destructureaza obiectul person pentru a obtine o variabila hasPet
+// si in functie de aceasta afiseaza propozitia: “Persoana are/nu are animale”
+const { petOwner: hasPet } = person;
+console.log(`Persoana ${hasPet === true ? 'are' : 'nu are'} animale.`);
+
+// 4.Destructureaza arrayul friends si obiectele de pe pozitia 0 si 2, astfel incat sa poti afisa propozitiile:
+// “Prietenul meu este Larry Larryson”
+// “Carol Carolson este prietena mea”. done :))^^^
+
+const [f0, , { name: f2Name, surname: f2Surname }] = person.friends;
+const { name: f0Name, surname: f0Surname } = f0;
+console.log(`Prietenul meu este ${f0Name} ${f0Surname}.`);
+console.log(`${f2Name} ${f2Surname} este prietena mea.`);
